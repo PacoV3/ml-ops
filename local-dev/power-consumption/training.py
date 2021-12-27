4f8531983b5124029bc8e08bbcb96e963b6e6c31
Francisco Ventura <franj.vents@gmail.com>
2021-12-27 23:41:41 +0000

    Change for prediction

--- local-dev/power-consumption/training.py
+++ local-dev/power-consumption/training.py
@@ -1,4 +1,4 @@
-# Imports to get Data
+## Get the data
 from azure.storage.blob import BlobServiceClient
 import pandas as pd
 import numpy as np
@@ -18,20 +18,20 @@ ap.add_argument('-m', '--model', type=str, default='power_model.model',
 	help='name for the trained model')
 args = vars(ap.parse_args())
 
-connect_str = args['key']
-azure_client = args['client']
+connect_str = args["key"]
+azure_client = args["client"]
 
 blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 container_client = blob_service_client.get_container_client(azure_client)
 
-file_name = args['file']
-data_path = '.data'
+file_name = args["file"]
+data_path = ".data"
 
 if not os.path.exists(data_path):
     os.mkdir(data_path)
 download_file_path = os.path.join(data_path, file_name)
 
-azure_path = args['blob']
+azure_path = args["blob"]
 
 blob_client = container_client.get_blob_client(os.path.join(azure_path, file_name))
 with open(download_file_path, "wb") as download_file:
@@ -44,12 +44,13 @@ df = pd.read_csv(download_file_path, sep=';', low_memory=False,
 df['Sub_metering_4'] = (df.iloc[:,0] * 1000 / 60) - (df.iloc[:,4] + df.iloc[:,5] + df.iloc[:,6])
 df.sort_values(by=["Date_time"], inplace=True)
 
+## Save processed data
 # processed_path = '.processed'
 # if not os.path.exists(processed_path):
 #     os.mkdir(processed_path)
 # df.to_csv(os.path.join(processed_path, 'household-pow.csv'))
 
-# Training
+## Training
 
 # Imports for model stats
 from sklearn.preprocessing import MinMaxScaler
@@ -70,6 +71,7 @@ droping_list_all
 for j in range(0, 8):
     df.iloc[:, j] = df.iloc[:, j].fillna(df.iloc[:, j].mean())
 df.isnull().sum()
+
 
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
     n_vars = 1 if type(data) is list else data.shape[1]
@@ -94,25 +96,26 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
         agg.dropna(inplace=True)
     return agg
 
+
 ## resampling of data over hour
 df_resample = df.resample("h").mean()
 df_resample.shape
 
 # values = df.values
-values = df_resample.values 
+values = df_resample.values
 # ensure all data is float
-values = values.astype('float32')
+values = values.astype("float32")
 # normalize features
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled = scaler.fit_transform(values)
 # frame as supervised learning
-reframed = series_to_supervised(scaled, 1, 1)   
+reframed = series_to_supervised(scaled, 1, 1)
 # drop columns we don't want to predict
-reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
+reframed.drop(reframed.columns[[9, 10, 11, 12, 13, 14, 15]], axis=1, inplace=True)
 # split into train and test sets
 values = reframed.values
 
-n_train_time = 365*24
+n_train_time = 365 * 24
 train = values[:n_train_time, :]
 test = values[n_train_time:, :]
 # test = values[n_train_time:n_test_time, :]
@@ -143,20 +146,19 @@ history = model.fit(
     shuffle=False,
 )
 
-# make a prediction
+## Make a prediction
 yhat = model.predict(test_X)
 new_test_X = test_X.reshape((test_X.shape[0], 8))
-# invert scaling for forecast
 inv_yhat = np.concatenate((yhat, new_test_X[:, -7:]), axis=1)
 inv_yhat = scaler.inverse_transform(inv_yhat)
 inv_yhat = inv_yhat[:, 0]
-# invert scaling for actual
 new_test_y = test_y.reshape((len(test_y), 1))
 inv_y = np.concatenate((new_test_y, new_test_X[:, -7:]), axis=1)
 inv_y = scaler.inverse_transform(inv_y)
 inv_y = inv_y[:, 0]
-# calculate RMSE
+
+# Calculate RMSE
 rmse = np.sqrt(mean_squared_error(inv_y, inv_yhat))
 print("Test RMSE: %.3f" % rmse)
 
-model.save(args['model'], save_format="h5")
+model.save(args["model"], save_format="h5")
